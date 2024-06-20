ca49eb3852018acdfb591e2a38433f1e62262406
= <=>
2024-06-20 12:41:39 +0200

    feat : added the verify ,method for the feat

--- routers/authentication.py
+++ routers/authentication.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter , Depends , HTTPException , Response , status
-import schemas , database , models
+import schemas , database , models , hashing
 from sqlalchemy.orm import Session
 
 router = APIRouter(tags=["Authentication"])
@@ -9,4 +9,8 @@ def login(requestBody:schemas.Login , db:Session =Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == requestBody.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail="Invalid Credentials")
+    
+    if not hashing.Hash.verify(requestBody.password , user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail="Invalid Credentials")
+
     return user
